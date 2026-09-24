01b29ccb18ab76ee39edb1715d53c6f0ea91b60f
S0rek1X5 <danielbebon@gmail.com>
2026-09-23 22:48:54 -0500

    Last one

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://S0rek1X5.github.io', 
   base: '/Prototype_1/',
   vite: {
     plugins: [tailwindcss()]
